6b9ff2048cdda880b353922646ca245d153509c6
Mark Lai <mark.lai@netccentric.com>
2013-11-12 16:32:47 +0800

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomnomadmin::Application.config.secret_key_base = '339bed2fb4ccf86125b594f7e0fb6efe3e55bf3d965424d5299f22d82fc05886a781c3e5f8d4cf4e28d5bd2939bda0c64e40b487ae0a19c87617cc56d141f726'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nomnomadmin::Application.config.session_store :cookie_store, key: '_nomnomadmin_session'
